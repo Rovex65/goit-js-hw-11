1430272087304daf0b48cb83f979cd06e6a01505
Rovex65 <107561862+Rovex65@users.noreply.github.com>
2024-02-11 14:39:02 +0000

    Deploying to gh-pages from @ Rovex65/goit-js-hw-11@2fa95b6770326f49b36e4032ca0a8cec4b170f13 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
 import{S as c,i as l}from"./assets/vendor-7659544d.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();class u{constructor(){this.BASE_URL="https://pixabay.com/api/",this.API_KEY="42308857-af265c382d366c65500552078"}getImages(o){const s=new URLSearchParams({key:this.API_KEY,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}),i=`${this.BASE_URL}?${s}`;return fetch(i).then(e=>e.json())}}const h=document.querySelector(".gallery"),d=new c(".gallery a");function f({webformatURL:t,largeImageURL:o,tags:s,likes:i,views:e,comments:r,downloads:a}){return`      
       <li class="card">
-        <a h ="${o}">
+        <a href ="${o}">
         <img class="card-img" src="${t}" alt="${s}" >
         <ul class="card-data">
           <li><h3>Likes</h3><p>${i}</p></li>
